d220aa88bd6e8015b8bc15bbb1d948c5f8e242d8
Pedro <51714168+zechx@users.noreply.github.com>
2024-07-24 16:37:56 -0300

    Dark Mode Styled to darkmode.

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <main>
       <div className="mx-auto mb-12 max-w-lg text-center">
         <div className="m-4">
-          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">Demo Protocolo Inteligente 
+          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-neutral-100 md:text-5xl lg:text-5xl">Demo Protocolo Inteligente 
             (Assistant Api + Blumo)</h1>
           <div className="mb-6 text-normal font-normal text-gray-500">
-            Esta es una demo ilustrativa de un asistente personalizado de OpenAI, ID: <a href="https://platform.openai.com/assistants/asst_MytRz2ceXk1VuijV2BwRg8Uz" target="_new" className="underline text-blue-500">asst_MytRz2ceXk1VuijV2BwRg8Uz</a>.  
+            Esta es una demo ilustrativa de un asistente personalizado de OpenAI, ID: <a href="https://platform.openai.com/assistants/asst_MytRz2ceXk1VuijV2BwRg8Uz" target="_new" className="underline text-yellow-400">asst_MytRz2ceXk1VuijV2BwRg8Uz</a>.  
             El asistente puede ser modificado con el link anterior.  
           </div>
         </div>
--- app/ui/openai-assistant.tsx
+++ app/ui/openai-assistant.tsx
@@ -128,7 +128,7 @@ export default function OpenAIAssistant({
     }
 
     return (
-        <div className="flex flex-col bg-slate-200 shadow-md relative">
+        <div className="flex flex-col bg-black shadow-md relative">
             <OpenAIAssistantMessage
                 message={greetingMessage}
             />
@@ -147,20 +147,20 @@ export default function OpenAIAssistant({
                 <input 
                     disabled={isLoading}
                     autoFocus
-                    className="border rounded w-full py-2 px-3 text-gray-70" 
+                    className="border focus:outline-none focus:ring-0 focus:border-white bg-neutral-800 rounded w-full py-2 px-3 text-white" 
                     onChange={handlePromptChange}
                     value={prompt}
                     placeholder="" />
                 {isLoading ? 
                     <button 
                         disabled
-                        className="ml-2  bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">   
+                        className="ml-2  bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">   
                         <OpenAISpinner /> 
                     </button>
                     : 
                     <button 
                         disabled={prompt.length == 0}
-                        className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">   
+                        className="ml-2 bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:shadow-yellow-500">   
                         <AiOutlineSend /> 
                     </button>
                 }
@@ -180,7 +180,7 @@ export function OpenAIAssistantMessage({message}: {message:Message}) {
         }
     }
     return (
-        <div className="flex rounded text-gray-700 text-center bg-white px-4 py-2 m-2 shadow-md">
+        <div className="flex rounded text-white text-center bg-zinc-800 px-4 py-2 m-2 shadow-md">
             <div className="text-4xl">
                 {displayRole(message.role)}
             </div>
@@ -196,9 +196,9 @@ export function OpenAIAssistantMessage({message}: {message:Message}) {
 // Based on https://flowbite.com/docs/components/spinner/
 function OpenAISpinner() {
     return (
-        <svg aria-hidden="true" role="status" className="inline w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-        </svg>    
+        <svg aria-hidden="true" role="status" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg> 
     )
 }
